66b2e2fbe5c632c6d6884c7580b210b9737ead8a
Anderson Luis <and.luis35@gmail.com>
2025-07-04 12:51:30 -0300

    Adicionado CSS básico na formatação das disciplinas

--- disciplinas-periodo/disciplinas.js
+++ disciplinas-periodo/disciplinas.js
@@ -3,7 +3,7 @@ let divDisciplinas = document.querySelector("#disciplinas");
 fetch("./disciplinas.json").then((response) => {
     response.json().then((info) => {
         info.disciplinas.map((disciplina) => {
-            divDisciplinas.innerHTML += `<li> CÓDIGO: ${disciplina.codigo} <br> NOME: ${disciplina.nome} <br> TURMA: ${disciplina.turma} <br> HORÁRIO: ${disciplina.horario} <br> EMENTÁRIO: ${disciplina.link}</li>`
+            divDisciplinas.innerHTML += `<li> CÓDIGO: ${disciplina.codigo} <br> NOME: ${disciplina.nome} <br> TURMA: ${disciplina.turma} <br> HORÁRIO: ${disciplina.horario} <br> EMENTÁRIO: <a href="${disciplina.link}">${disciplina.link}</a></li>`
         })
     })
 })
