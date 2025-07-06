fe8eeec4d507c9c9ef3284200f0337cb1de7f87c
God-rep <gabrielgodoysilva@hotmail.com>
2025-07-06 01:57:10 -0300

    Estrutura inicial do script

--- discentes-com-matricula-ativa-na-disciplina/discentes-inscritos.js
+++ discentes-com-matricula-ativa-na-disciplina/discentes-inscritos.js
@@ -1,1 +1,34 @@
+fetch('../disciplinas-perioso/turmas.json') // Adjust the path as needed
+  .then(response => response.json())
+  .then(turmas => {
+    console.log(turmas);
+  })
+  .catch(error => console.error('Error loading JSON:', error));
+
+
+
+
+
+
+
+//Inicia página pela primeira vez
+function iniciaPag(){}
+
+
+
+//Array de todos os alunos na disciplina
+var alunosTabela = []
+
+//Posição do primeiro aluno da visualização atual da tabela no array de alunos 
+var alunoCimaTabela = 0
+  
+//"< 10 Anteriores"
+function antPag(){}
+
+//"Proximos 10 >"
+function proxPag(){}
+
+
+//Aciona ao clicar nos botões de cada disciplina
+function mudaDisciplina(){}
 
