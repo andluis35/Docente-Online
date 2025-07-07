71e63ddc158ad7349499cc911962e9a92e6b645d
GabryelJ <gabryeljuliods@gmail.com>
2025-07-07 19:09:54 -0300

    Script de gerencia de  contexto da tablea

---
+++ script/layouts/trocaContextoGradeDiscentesERegistros.js
@@ -0,0 +1,46 @@
+/**
+ * Este módulo gerencia a troca de contexto de visualização em uma página,
+ * alternando entre "Discentes com Matrícula Ativa" e "Registro de Notas e Faltas".
+ * Ele manipula dinamicamente o texto de um botão e a visibilidade de elementos
+ * de classe css 'celula-registros' na interface do usuário usando jQuery.
+ */
+
+$(function() {
+    let isDiscentesAtivo = false;
+
+    function exibirVersaoDiscentes() {
+        isDiscentesAtivo = true;
+        console.log('Contexto: Discentes com Matrícula Ativa');
+        $('#troca-contexto').text('Registro de Notas e Faltas');
+        $('.celula-registros').hide();
+    }
+
+    function exibirVersaoRegistros() {
+        isDiscentesAtivo = false;
+        console.log('Contexto: Registro de Notas e Faltas');
+        $('#troca-contexto').text('Discentes com Matrícula Ativa');
+        $('.celula-registros').show();
+    }
+
+
+    $('#troca-contexto').on('click', function(event) {
+        event.preventDefault();
+
+        if (isDiscentesAtivo) {
+            exibirVersaoRegistros();
+        } else {
+            exibirVersaoDiscentes();
+        }
+    });
+
+
+    $(document).on('tabelaAtualizada', function(event) {
+        if (isDiscentesAtivo) {
+            exibirVersaoDiscentes();
+        }else{
+            exibirVersaoRegistros();
+        }
+    });
+
+    exibirVersaoRegistros();
+});
