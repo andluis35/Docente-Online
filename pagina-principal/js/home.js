41ae9b9775856c6197fcdd19bb373bd8d86462f5
Mateus Souza <61070878+MatteusSouza@users.noreply.github.com>
2025-07-07 23:29:04 -0300

    navegacao usando header e footer

--- pagina-principal/js/home.js
+++ pagina-principal/js/home.js
@@ -1,53 +1,7 @@
-import { getSession, endSession } from "../../auth/auth.js";
-import { Navigate } from "../../route/routes.js";
+// import { Navigate } from "../../route/routes.js";
 
-const userData = getSession();
-
-if (!userData) {
-  Navigate.root()
-}
-
-$(document).on('click', '#btn-profile', function(event) {
+$(document).on('click', '#btn-calendario-academico', function(event) {
   event.preventDefault();
-  alert(
-    'Nome: ' + userData.nome+ '\n' +
-    'CPF: ' + userData.cpf + '\n' +
-    'Matrícula: ' + userData.matricula
-  );
-  console.log('Botão profile clicado.');
+  console.log("calendario academico")
 });
 
-$(document).on('click', '#btn-logout', function(event) {
-  event.preventDefault();
-  endSession();
-});
-
-$(document).on('click', '#btn-disciplinas-periodo', function(event) {
-  event.preventDefault(); // Impede que o '#' seja adicionado à URL
-  Navigate.disciplinas();
-});
-
-$(document).on('click', '#btn-discentes-ativos', function(event) {
-  event.preventDefault();
-  Navigate.discentes();
-});
-
-$(document).on('click', '#btn-registro-notas', function(event) {
-  event.preventDefault();
-  Navigate.notasEFaltas();
-});
-
-$(document).on('click', '#footer-btn-disciplinas', function(event) {
-  event.preventDefault();
-  console.log('Botão de rodapé "Disciplinas" clicado.');
-});
-
-$(document).on('click', '#footer-btn-discentes', function(event) {
-  event.preventDefault();
-  console.log('Botão de rodapé "Discentes" clicado.');
-});
-
-$(document).on('click', '#footer-btn-registro', function(event) {
-  event.preventDefault();
-  console.log('Botão de rodapé "Registro" clicado.');
-});
