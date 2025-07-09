6835736d992287f4d281aae4c2b32ce4cbd42c7f
God-rep <gabrielgodoysilva@hotmail.com>
2025-07-08 22:30:27 -0300

    Update formulario.js

--- registro-de-notas-e-faltas/formulario.js
+++ registro-de-notas-e-faltas/formulario.js
@@ -61,17 +61,11 @@ function salvaFormulario(turma){
 		
 		let aluno = turma.alunos.find(aluno => aluno.matricula == matricula)	// Procura aluno na lista de alunos da turma usando matricula
 
-		let notaP1 = (child.children[4].value == "")?aluno.notas.P1:child.children[4].children[0].value;
-		let notaP2 = (child.children[5].value == "")?aluno.notas.P2:child.children[5].children[0].value;
-		let notaPF = (child.children[6].value == "")?aluno.notas.PF:child.children[6].children[0].value; 
+		let notaP1 = (child.children[4].value === "")?aluno.notas.P1:child.children[4].children[0].value;
+		let notaP2 = (child.children[5].value === "")?aluno.notas.P2:child.children[5].children[0].value;
+		let notaPF = (child.children[6].value === "")?aluno.notas.PF:child.children[6].children[0].value; 
 		
 		let notaMF = (notaP1+notaP2)/2;
-
-		console.log("child atual:", child);
-		console.log("P1:", notaP2);
-		console.log("P2:", notaP1);
-		console.log("MF:", notaMF);
-
 		
 		aluno.notas.P1 = notaP1;
 		aluno.notas.P2 = notaP2;
@@ -102,18 +96,22 @@ function checaFormulario(){
 		let aluno = turma.alunos.find(aluno => aluno.matricula === matricula)	// Procura aluno na lista de alunos da turma usando matricula
 
 
-		let notaP1 = (child.children[4].value == "")?aluno.notas.P1:child.children[4].children[0].value;
-		let notaP2 = (child.children[5].value == "")?aluno.notas.P2:child.children[5].children[0].value;
-		let notaPF = (child.children[6].value == "")?aluno.notas.PF:child.children[6].children[0].value; 
+		let notaP1 = (child.children[4].value === "")?aluno.notas.P1:child.children[4].children[0].value;
+		let notaP2 = (child.children[5].value === "")?aluno.notas.P2:child.children[5].children[0].value;
+		let notaPF = (child.children[6].value === "")?aluno.notas.PF:child.children[6].children[0].value; 
 
 		let notaMF = (notaP1+notaP2)/2;
 		
+		console.log("child atual:", child);
+		console.log("P1:", notaP2);
+		console.log("P2:", notaP1);
+		console.log("MF:", notaMF);
 		
-		if((notaMF>=7) && (notaPF !== 0)){ //Aluno aprovado direto nao pode ter PF
+		if((notaMF>=7) && (notaPF != 0)){ //Aluno aprovado direto nao pode ter PF
 			throw new Error("Nota PF diferente de 0, mas P1 e P2 já aprovam! ((P1+P2)/2 >=7)");
 		}
 		
-		if((notaMF<4) && (notaPF !== 0)){	//Aluno reprovado direto nao pode ter PF
+		if((notaMF<4) && (notaPF != 0)){	//Aluno reprovado direto nao pode ter PF
 			throw new Error("Nota PF diferente de 0, mas aluno já está reprovado! ((P1+P2)/2 <4)");
 		}
 		console.log("Aluno checado!");
