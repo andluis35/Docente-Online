142d0e7cb10948e0db216d7c76d12f7aa29530e9
God-rep <gabrielgodoysilva@hotmail.com>
2025-07-10 02:19:43 -0300

    Implementação lógica de checagem e salvar faltas do formulário

--- registro-de-notas-e-faltas/formulario.js
+++ registro-de-notas-e-faltas/formulario.js
@@ -73,6 +73,7 @@ function salvaFormulario(turma){
 		aluno.notas.P2 = notaP2;
 		aluno.notas.PF = notaPF;
 		aluno.notas.mediaFinal = notaMF;
+		aluno.faltas = child.children[8].children[0].value;
 		console.log("Aluno salvo!");
 	}
 	// Salva JSON atualizado no localStorage
@@ -97,6 +98,11 @@ function checaFormulario(){
 		let aluno = turma.alunos.find(aluno => aluno.matricula == matricula)	// Procura aluno na lista de alunos da turma usando matricula
 
 
+		
+						/* CHECA NOTA */
+
+
+		
 		let notaP1 = (child.children[4].children[0].value === "")?aluno.notas.P1:child.children[4].children[0].value;
 		let notaP2 = (child.children[5].children[0].value === "")?aluno.notas.P2:child.children[5].children[0].value;
 		let notaPF = (child.children[6].children[0].value === "")?aluno.notas.PF:child.children[6].children[0].value; 
@@ -131,6 +137,15 @@ function checaFormulario(){
 		if((notaMF<4) && (notaPF != -0.1)){	//Aluno reprovado direto nao pode ter PF
 			throw new Error("Nota PF diferente de N/A, mas P1 e P2 já reprovam sozinhas! ((P1+P2)/2 <4). Aluno ", child.children[0].textContent);
 		}
+
+				/* CHECA FALTAS */
+
+		let faltas = (child.children[8].children[0].value === "")?aluno.notas.PF:child.children[8].children[0].value;
+
+		if(faltas < 0){
+			throw new Error("Número de faltas não pode ser negativo!");
+		}
+		
 		console.log("Aluno checado!");
 	}
 	console.log("Toda a turma foi checada!");
