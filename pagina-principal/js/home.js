100da4b50800002258a87088d09c8ffd137dd46b
God-rep <gabrielgodoysilva@hotmail.com>
2025-07-10 20:04:40 -0300

    Mudado nome de funções na chamada (estava errado)

--- pagina-principal/js/home.js
+++ pagina-principal/js/home.js
@@ -1,12 +1,8 @@
 // import { Navigate } from "../../route/routes.js";
-import { getDocenteDisciplinas } from "../../script/buscarDados.js";
+import { getDocenteTurmas } from "../../script/buscarDados.js";
 
 $(document).on('click', '#btn-calendario-academico', function(event) {
   console.log("calendario academico");
 });
 
-let disciplinasCarregadas = false;
-if (!disciplinasCarregadas){
-  let docenteDisciplinas = getDocenteDisciplinas(usuarioLogado.matricula);
-  disciplinasCarregadas = true;
-}
+let docenteTurmas = getDocenteTurmas(usuarioLogado.matricula);
