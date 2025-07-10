1f21515e4243415a155fc8ca8de8ddb15d830381
God-rep <gabrielgodoysilva@hotmail.com>
2025-07-10 20:15:22 -0300

    Update home.js

--- pagina-principal/js/home.js
+++ pagina-principal/js/home.js
@@ -1,5 +1,24 @@
-// import { Navigate } from "../../route/routes.js";
+import { Navigate } from "../../route/routes.js";
 import { getDocenteTurmas } from "../../script/buscarDados.js";
+import { getSession } from "../../auth/auth.js";
+
+const usuarioLogado = getSession();
+if (!usuarioLogado) {
+  Navigate.root()
+}
+
+
+export function getSession() {
+    /**Obtem os dados do usuario logado salvos no localStorage 
+    * Retorna um objeto: { nome: string, cpf: string, matricula: string } */
+    const userAuth = localStorage.getItem('usuarioAutenticado');
+
+    if (!userAuth) {
+        return null
+    }
+    
+    return JSON.parse(userAuth);
+}
 
 $(document).on('click', '#btn-calendario-academico', function(event) {
   console.log("calendario academico");
