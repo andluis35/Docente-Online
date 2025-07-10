1c6a94723c495b272dc6825803b15e996c9fa513
God-rep <gabrielgodoysilva@hotmail.com>
2025-07-10 19:55:38 -0300

    Update home.js
    -> pega as disciplinas do usuario logado no momento que entra pela primeira fez na página principal.

--- pagina-principal/js/home.js
+++ pagina-principal/js/home.js
@@ -1,6 +1,12 @@
 // import { Navigate } from "../../route/routes.js";
+import { getDocenteDisciplinas } from "../../script/buscarDados.js";
 
 $(document).on('click', '#btn-calendario-academico', function(event) {
-  console.log("calendario academico")
+  console.log("calendario academico");
 });
 
+let disciplinasCarregadas = false;
+if (!disciplinasCarregadas){
+  let docenteDisciplinas = getDocenteDisciplinas(usuarioLogado.matricula);
+  disciplinasCarregadas = true;
+}
