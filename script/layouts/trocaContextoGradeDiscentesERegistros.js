99417065846ca470ae07469a6adbd60617a9c99c
GabryelJ <gabryeljuliods@gmail.com>
2025-07-09 21:32:02 -0300

    Ajuste doc, botão SAG.

--- script/layouts/trocaContextoGradeDiscentesERegistros.js
+++ script/layouts/trocaContextoGradeDiscentesERegistros.js
@@ -3,6 +3,8 @@
  * alternando entre "Discentes com Matrícula Ativa" e "Registro de Notas e Faltas".
  * Ele manipula dinamicamente o texto de um botão e a visibilidade de elementos
  * de classe css 'celula-registros' na interface do usuário usando jQuery.
+ * O módulo aguarda um evento lançado pelo script (registro-de-notas-e-faltas/script.js)
+ * que atualiza a tabela. Ao detectar o evento, atualiza a visualização de acordo com o modo atual.
  */
 
 $(function() {
