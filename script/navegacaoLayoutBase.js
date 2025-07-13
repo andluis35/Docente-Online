1efe4636c75efbd213f41e8e5dda283689e2598b
GabryelJ <gabryeljuliods@gmail.com>
2025-07-12 22:04:17 -0300

    Ajustes navegação

--- script/navegacaoLayoutBase.js
+++ script/navegacaoLayoutBase.js
@@ -84,11 +84,15 @@ $(document).on('click', '.btn-disciplinas-periodo', function(event) {
 
 $(document).on('click', '.btn-discentes-ativos', function(event) {
   event.preventDefault();
-    Navigate.notasEFaltas();
+  localStorage.setItem("isDiscentesAtivo", 'true')
+  Navigate.controleDiscentes();
+
 });
 
 $(document).on('click', '.btn-registro-notas', function(event) {
   event.preventDefault();
+  localStorage.setItem("isDiscentesAtivo", 'false')
+  Navigate.controleDiscentes();
   Navigate.notasEFaltas();
 });
 
